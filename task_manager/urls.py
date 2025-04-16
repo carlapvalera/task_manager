7c2632fa229213934c6cb1034e8eb3303057fa34
carlapvalera <perezvaleracarlas@gmail.com>
2025-04-16 06:25:37 -0400

    requirement.txt added

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -29,16 +29,3 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
 
-"""
-from django.urls import path
-from .views import ProjectList, ProjectDetail, TaskList, TaskDetail, RemoveLeaderView, RemoveUserFromProjectView
-
-urlpatterns = [
-    path('projects/', ProjectList.as_view()),
-    path('projects/<int:pk>/', ProjectDetail.as_view()),
-    path('projects/<int:project_id>/tasks/', TaskList.as_view()),
-    path('tasks/<int:pk>/', TaskDetail.as_view()),
-    path('projects/<int:project_id>/remove-leader/', RemoveLeaderView.as_view()),
-    path('projects/<int:project_id>/remove-user/<int:user_id>/', RemoveUserFromProjectView.as_view()),
-]
-"""
